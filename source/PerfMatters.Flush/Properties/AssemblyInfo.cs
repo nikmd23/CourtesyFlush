9128eed602a712dd42b274a214ca091ca9097a73
Nik Molnar <nikmd23@gmail.com>
2014-03-17 16:53:58 -0400

    Setup simple build with `nuget pack` command

--- source/PerfMatters.Flush/Properties/AssemblyInfo.cs
+++ source/PerfMatters.Flush/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PerfMatters.Flush")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PerfMatters.Flush")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyCompany("Nik Molnar")]
+[assembly: AssemblyDescription("A library to simplify flushing early in ASP.NET MVC")]
